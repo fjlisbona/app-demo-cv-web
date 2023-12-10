30de31217e8e47649e003862f2c269c0676ae298
fjlisbona <31550431+fjlisbona@users.noreply.github.com>
2023-12-10 21:39:59 +0100

    Web app completed!

---
+++ assets/i18n/en.js
@@ -0,0 +1,136 @@
+export const workingExperience=()=>{
+ return  `Work Experience
+
+>Java Developer at Serbatic (Mutua Madrileña) (12/2022-06/2023).
+-Increased test coverage in reports using the Jacoco and JUnit testing tools.
+-Analyzed and implemented improvements in optimizing timeouts caused by specific queries, including form validation for the query.
+-Restructured and analyzed existing code and added new validation functionality.
+-Analyzed tasks to optimize product quality, such as reducing log traces.
+-Migrated SOAP API to REST.
+-Conducted method refactoring and code optimization with Java 11.
+REST - Spring web Flow-Java 11- Spring Boot-Apache Tiles- Scrum Agile-Git(GitFlow)-Jenkins-Sonar -Openshift-JSP-Taglibs -Jira-Jacoco-Junit5-Spring Tools Suite-HTML-CSS-SQL:DB2.
+
+>Java Swing Developer at Dedalus Group(DXC Technology)( 01/2021- 30/11/2022).
+
+- Participated in version creation by developing requirements and resolving incidents for production environments of the company's product.
+- Created methods in Web Services.
+- Handled Jasper Report reports using the Ireport tool.
+- Utilized data with Oracle technology.
+- Managed code versioning system with Subversion.
+- Conducted testing of tasks performed by other colleagues.
+Java Swing- Apache Ant -Oracle-Subversion-Jasper report.
+
+>Full-Stack Developer at TourDiez Travel( 10/2019-04/2020)
+
+- Automated processes using Python, extracting data from Oracle and MySQL databases, and utilizing the Pandas library.
+- Developed new features and fixes in the application using Java, JSP, TagsLib, HTML, CSS, and JavaScript technologies.
+- Utilized frameworks: Twitter Bootstrap and JQuery.
+- Used Subversion and Git as version control systems.
+- Ventus Proxy: Assigned new clients in the configuration rule for load balancing of requests on Apache Tomcat servers.
+- Implemented HAProxy as a load balancer and request caching system on the front end.
+- Created events and functions with JavaScript for data collection using Google Analytics.
+
+JavaEE- Python-Oracle-Mysql-Git-Tomcat-Javascript-HTML-CSS
+
+> Java Developer at Grupo AL.(12/2018-6/2019)
+
+- Java function that sent a backup copy of a directory to an FTP server using the Apache Commons library.
+- Created and fixed reports using Crystal Reports and Ireport.
+- Front-end development: interactive components using JSF along with HTML and CSS technologies.
+- Created components for the automation of the barcode reading process using an ls-2208 scanner with Icefaces components.
+- Created functions within the persistence layer using JPA.
+
+J2EE -JSF-Icefaces-Glassfish – Wildfly -JPA -MySql-Git-Crystal
+report-Jasper Report-Maven-HTML-CSS-Javascript.
+
+
+>Java Developer at Lucatic(2/2018).
+
+- Analysis of database field changes and their impact on the project.
+- Removal of fields or attributes and business logic in the controller part.
+- Resolution of issues passing through the different layers of the application in a Microservices architecture.
+Java-Spring-Jquery.
+
+
+>  Java Developer at Accenture – Málaga(6 meses, 2016-2017)
+
+RACC
+
+-Implementation of changes in the application and issue resolution.
+-Monitoring and control of Batch processes.
+Java 8-PL SQL
+
+ Hotelbeds Group
+	-Processing emails using the javax Mail API, including emails with attachments.
+-Utilizing MongoDB technology for data persistence.
+-Handling issue resolution and implementing changes within a Microservices environment.
+
+Weblogic-Spring Boot-Java 8-MongoDB-Jira-Scrum
+
+>Java Developer Internship at Esail’s Solutions(4/2016-7/2016).Málaga
+
+-Creation of a management module,standalone Java application , that read data from XML files and provided data output in Excel files.
+-Project involving Spring Boot, MyBatis, and AngularJS.
+
+Java - XML - Librería Apache POI - Spring Boot-MyBatis- Spring Data JPA-AngularJS
+-Twitter Bootstrap – Javascript – HTML – CSS.
+
+> Desarrollador Java
+ Talanx Systeme AG (Colonia, Alemania)
+
+-Product: "TADEAS - Insurance Sales Offers System."
+-Creation of a new JAVA EE application: the calculation module of the TADEAS offers system. This JAVA application acted as a client for a RESTful Web Service. This Web Service returned data in JSON format, facilitated by the Jersey library.
+-Conducted testing using the Testng Framework to test various functionalities of the application.
+
+J2EE- JSF - Jersey - Javascript - XHTML y
+Twitter Bootstrap(CSS) – TestNG-REST.`
+};
+export const learningTraining=()=>{
+ return  `Academic Background
+  • Google certificate: "Building Microservices with Spring Boot and Google Cloud Platform" (2020).
+  • Incentive Training Course at IMFE (6-8/2017), Málaga
+- PHP
+- MYSQL
+- HTML5 and CSS3
+- JQUERY
+  • Incentive Training Course at IMFE for Java and Spring (11/2015-2/2016), Málaga
+- J2EE
+- Spring Framework (Spring MVC, Spring Boot)
+- MySQL
+- Javascript, HTML, and CSS
+
+
+  • Higher Technician in Web Application Development (2013-2015)
+VET Campanillas - Málaga
+
+-Java
+- PHP
+- Relational Database Design
+- Query Languages: SQL, MySQL, and PL-SQL
+- Software Analysis: Metrica v3
+- HTML5 and CSS3, Javascript, AngularJS, and jQuery
+
+  • CCNA (Cisco Certified Networking Associate) (2012) VET Campanillas - Málaga
+  • Professional Certificate in Java and C Applications (2011-2012)  Aesita Academy- Málaga
+  • Server and Web Page Administrator Course (2011)
+Lehmberg Formación - Málaga
+- HTML
+- CSS
+- JavaScript
+-  PHP
+- MYSQL
+Internship as a programmer at Linkasoft after the Server and Web Page Administrator course (3 months). Work environment: E-commerce with "Lekommerce."`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
---
+++ assets/i18n/es.js
@@ -0,0 +1,170 @@
+export const workingExperience=()=>{
+ return  `Experiencia laboral
+>Desarrollador Java Microservicios(12/2022-06/2023)
+en Serbatic( Mutua Madrileña)
+• Aumento de la cobertura de test en informes con la herramienta Jacoco y JUnit.
+• Validación de los valores de un formulario comprobando si son iguales a los de la aplicación
+con respecto a los que vienen del formulario.
+Restructuración y análisis de código ya existente y añadido de nueva funcionalidad de
+validación.
+• Análisis de tareas para optimizar la calidad del producto como reducción de traza en los
+logs.
+• Análisis e implementación de mejoras en la optimización de timeouts producidos por query
+especifica. Incluida validación del formulario de la query.
+• Migracion de api SOAP a REST.
+• Tareas de Refactorización de métodos y optimización de código con java 11.
+REST - Spring web Flow-Java 11- Spring Boot-Apache Tiles- Scrum Agile-Git(GitFlow)-Jenkins-
+Sonar -Openshift-JSP-Taglibs -Jira-Jacoco-Junit5-Spring Tools Suite-HTML-CSS-SQL:DB2.
+>Desarrollador Java Swing( 01/2021- 30/11/2022)
+en Dedalus Group(DXC Technology).
+• Participación en la creación de versiones mediante el desarrollo de requisitos y arreglo de
+incidencias para entornos de producción del producto de la empresa.
+• Creación de métodos en Web Services.
+• Tratamiento de Informes Jasper Report con la herramienta Ireport.
+• Utilización de datos con tecnología Oracle.
+• Manejo de Sistema de versionado de código con Subversión.
+• Pruebas de tareas realizadas por otros compañeros.
+Java Swing- Apache Ant -Oracle-Subversion-Jasper report.
+>Programador Full-Stack(10/2019-04/2020)
+TourDiez Travel
+• Automatización de procesos con Python extrayendo datos con base de datos Oracle y Mysql y
+librería Pandas.
+• Creación de nuevos desarrollos y arreglos en la aplicación con tecnología
+Java,JSP,TagsLib,HTML,CSS y Javascript.
+• Utilización de framework: Twitter Bootstrap y JQuery.
+• Utilización de Subversion y Git como sistema de control de versiones.
+• Ventus Proxy: Asignación de nuevos clientes en la configuración de la regla para el
+balanceo de carga de peticiones en los servidores Apache Tomcat.
+• Uso de HAProxy como balanceador de carga y cacheo de peticiones en la parte
+Front.
+• Creación de eventos y funciones con javascript para recolección de datos con Google Analytics.
+JavaEE- Python-Oracle-Mysql-Git-Tomcat-Javascript-HTML-CSS
+>Programador Java(12/2018-6/2019)
+en Grupo AL.
+• Función Java que enviaba una copia de seguridad de un directorio a un servidor
+FTP usando la librería Apache Commons.
+• Creación y arreglo de Informes en Crystal reports y Ireport.
+• Parte FrontEnd : componentes interactivos con tecnologia JSF junto a HTML y CSS.
+• Creación de componentes para la automatización de proceso de lectura de código
+de barras mediante lector ls-2208 con componentes de Icefaces.
+• Creación de funciones dentro de la capa de persistencia con JPA.
+J2EE -JSF-Icefaces-Glassfish – Wildfly -JPA -MySql-Git-Crystal
+report-Jasper Report-Maven-HTML-CSS-Javascript.
+>Desarrollador Java (2/2018)
+en Lucatic.
+• Análisis de cambio en campos de Base de datos e impacto en el proyecto.
+Supresión de campos o atributos y lógica de negocio en la parte del controlador.
+• Resolución de incidencias pasando a través de las diferentes capas de la aplicación
+en una arquitectura de Microservicios.
+Java-Spring-Jquery.
+> Desarrollador Java(6 meses, 2016-2017)
+en Accenture – Málaga
+Proyecto: RACC
+Implementación de cambios en el aplicativo y resolución de incidencias.
+Seguimiento de control de procesos Batch.
+Java-PL SQL
+Proyecto: Hotelbeds
+Procesamiento de correos electrónicos mediante la API Mail de javax
+usando correos con adjuntos y tecnología MongoDB para la persistencia
+de datos. Llevando a cabo resolución de incidencias e implementación de
+cambios dentro de un entorno Microservicios.
+Weblogic-Spring Boot-Java 8-MongoDB
+>Programador Java en prácticas
+en Esail’s Solutions(4/2016-7/2016).
+– Málaga
+• Creación de un módulo de gestión, aplicación Java ,que leía datos de archivo XML y
+que daba salida de datos en archivos Excel.
+• Proyecto con Spring Boot, MyBatis y AngularJS.
+Java - XML - Librería Apache POI - Spring Framework-MyBatis- Spring Data JPA-AngularJS
+-Twitter Bootstrap – Javascript – HTML – CSS.
+> Desarrollador Java
+Talanx Systeme AG (Colonia, Alemania)
+- Producto "TADEAS Sistema de ofertas para comerciales de seguros".
+-Creación de una nueva aplicación JAVA EE : módulo de cálculo del sistema de ofertas
+TADEAS. Esta aplicación JAVA era un cliente de Web Service Rest. Este Web Service
+devolvía datos en formato JSON, gracias a la librería Jersey.
+-Realización de Test con el Framework Testng, para probar varias funcionalidades del
+aplicativo.
+J2EE- JSF - Jersey - Realización de funciones de Javascript - Manejo de XHTML y
+Twitter Bootstrap(CSS) – TestNG.
+Preparación académica
+-Certificado Google :“Building Microservices with Spring Boot and Google Cloud
+Platform”(2020).
+-Curso formación incentivada IMFE(6-8/2017).Málaga
+• PHP
+• MYSQL
+• HTML5 y CSS3
+• JQUERY
+-Curso formación incentivada IMFE Java y Spring(11/2015-2/2016).Málaga
+• J2EE
+• Spring Framework(Spring MVC,Spring boot)
+• Mysql.
+• Javascript, HTML y CSS.
+-Técnico Superior en Desarrollo de Aplicaciones Web (2013-2015)
+IES Campanillas - Málaga
+• Java
+• PHP
+• Diseño de base de datos relacionales
+• Lenguaje de consultas: SQL, MySQL y PL-SQL
+• Análisis de software: metodologia metrica v3
+• HTML5 y CSS3, Javascript, AngularJS y Jquery
+-Certificación CCNA(Cisco Certified Networking Associate)(2012).
+IES Campanillas -Málaga
+-Certificado de profesionalidad con aplicaciones Java y C (2011-2012)
+Academia Aesita - Málaga
+-Curso de Administrador de servidores y páginas web. (2011)
+Lehmberg formación - Málaga
+• HTML
+• CSS
+• JAVASCRIPT
+• PHP
+• MYSQL
+Prácticas como programador en Linkasoft a partir del curso de Administrador de
+servidores y
+páginas web.(3 meses).
+Entorno de trabajo E-commerce con “Lekommerce”.`;
+};
+export const learningTraining=()=>{
+ return  `Preparación académica
+  -Certificado Google :“Building Microservices with Spring Boot and Google Cloud
+  Platform”(2020).
+  -Curso formación incentivada IMFE(6-8/2017).Málaga
+  • PHP
+  • MYSQL
+  • HTML5 y CSS3
+  • JQUERY
+  -Curso formación incentivada IMFE Java y Spring(11/2015-2/2016).Málaga
+  • J2EE
+  • Spring Framework(Spring MVC,Spring boot)
+  • Mysql.
+  • Javascript, HTML y CSS.
+  -Técnico Superior en Desarrollo de Aplicaciones Web (2013-2015)
+  IES Campanillas - Málaga
+  • Java
+  • PHP
+  • Diseño de base de datos relacionales
+  • Lenguaje de consultas: SQL, MySQL y PL-SQL
+  • Análisis de software: metodologia metrica v3
+  • HTML5 y CSS3, Javascript, AngularJS y Jquery
+  -Certificación CCNA(Cisco Certified Networking Associate)(2012).
+  IES Campanillas -Málaga
+  -Certificado de profesionalidad con aplicaciones Java y C (2011-2012)
+  Academia Aesita - Málaga
+  -Curso de Administrador de servidores y páginas web. (2011)
+  Lehmberg formación - Málaga
+  • HTML
+  • CSS
+  • JAVASCRIPT
+  • PHP
+  • MYSQL
+  Prácticas como programador en Linkasoft a partir del curso de Administrador de
+  servidores y
+  páginas web.(3 meses).
+  Entorno de trabajo E-commerce con “Lekommerce”.
+`;
+
+
+
+
+
+};
